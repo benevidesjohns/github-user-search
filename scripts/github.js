b4f4753f6afac0c202b7dd16a8aa8b52c30bbe2a
benevidesjohns <benevidesjohns@gmail.com>
2023-12-10 18:09:59 -0300

    chore: remove redundant input value check

--- scripts/github.js
+++ scripts/github.js
@@ -6,44 +6,41 @@ import { getUserData, getRepositories } from './apiRequests.js';
 // Executa as requisições da API e carrega
 // os dados dos usuários e seus repositórios na tela
 function loadData(username, buttonEl) {
-    // Verifica se existe algum valor no input text
-    if (username.value) {
-        // Limpa os elementos da tela
-        clearElements()
+    // Limpa os elementos da tela
+    clearElements()
 
-        // Apresenta o spinner para simular carregamento
-        spinner('inline-block')
+    // Apresenta o spinner para simular carregamento
+    spinner('inline-block')
 
-        // Executa as promises referentes às requisições
-        // do usuário e de seus repositórios simultaneamente
-        let promiseGithub = Promise.all([
-            getUserData(username.value),
-            getRepositories(username.value)
-        ])
+    // Executa as promises referentes às requisições
+    // do usuário e de seus repositórios simultaneamente
+    let promiseGithub = Promise.all([
+        getUserData(username.value),
+        getRepositories(username.value)
+    ])
 
-        promiseGithub
-            .then(([userData, repositoriesData]) => {
-                loadUserElements(userData)                          // Carrega os dados do usuário
-                loadRepositoriesElements(repositoriesData)          // Carrega os repositórios do usuário
+    promiseGithub
+        .then(([userData, repositoriesData]) => {
+            loadUserElements(userData)                          // Carrega os dados do usuário
+            loadRepositoriesElements(repositoriesData)          // Carrega os repositórios do usuário
 
-                spinner('none')     //Desativa o spinner
-            })
-            .catch(error => {
-                // Apresenta a área de erro para o usuário
-                document.querySelector('.github-gif-area').style.display = 'block'
+            spinner('none')     //Desativa o spinner
+        })
+        .catch(error => {
+            // Apresenta a área de erro para o usuário
+            document.querySelector('.github-gif-area').style.display = 'block'
 
-                let errorArea = document.querySelector('.custom-area.error')
-                showErrorMessage(errorArea)                         // Apresenta o erro para o usuário
+            let errorArea = document.querySelector('.custom-area.error')
+            showErrorMessage(errorArea)                         // Apresenta o erro para o usuário
 
-                console.error('Erro:', error);                      // Apresenta o erro no console
+            console.error('Erro:', error);                      // Apresenta o erro no console
 
-                spinner('none')     //Desativa o spinner
-            });
+            spinner('none')     //Desativa o spinner
+        });
 
-        // Limpa a pesquisa no input (inputSearch)
-        username.value = ''
-        buttonEl.disabled = true    // Desabilita o botão após a busca
-    }
+    // Limpa a pesquisa no input (inputSearch)
+    username.value = ''
+    buttonEl.disabled = true    // Desabilita o botão após a busca
 }
 
 function main() {
